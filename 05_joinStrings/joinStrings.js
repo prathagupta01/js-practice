7f77c19cbebae0cb6906ea257a82978332cfe614
prathagupta01 <gpratha466@gmail.com>
2025-12-09 00:15:37 +0530

    completed :) :tired_face: :heart:

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -8,7 +8,14 @@
 */
 
 // Add your code right below, good luck!
-
+ 
+let firstName = 'Carlos'
+let lastName = 'Stevenson'
+thisYear = 1965
+birthYear = 1947
+let fullName = firstName + ' ' + lastName
+let age = thisYear - birthYear
+let greeting = `Hello! My name is ` + fullName + ` and I am ` + age + ` years old.`
 
 
 
