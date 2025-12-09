e800fb74a373e61ef8c3ad2e0488e46ae754f551
prathagupta01 <gpratha466@gmail.com>
2025-12-09 21:02:13 +0530

    completed :blush: :memo:

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let len = str.length
+    let reverse =''
+    for (let i = len - 1; i >= 0; i--) {
+        reverse += str[i]
+    }
+    return reverse;
 };
 
 // Do not edit below this line
