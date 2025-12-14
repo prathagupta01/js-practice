1572263c745cfa816e5f57f2ca0476a6012d0be1
prathagupta01 <gpratha466@gmail.com>
2025-12-14 19:58:22 +0530

    completed :smile:

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...values) {
+
+// --------------------- FIRST METHOD -----------------------
+
+    // let neww = [];
+    // for(const val of arr){
+    //     if(!values.includes(val)) neww.push(val);
+    // }
+    // return neww;
+
+
+// --------------------- SECOND METHOD -----------------------
+
+    // return arr.filter(v => (!values.includes(v)));
+
+// --------------------- THIRD METHOD  - MY METHOD-----------------------
+
+    for(const val of values){
+        let index = arr.indexOf(val);
+        while(index !== -1){
+            arr.splice(index,1);
+            index = arr.indexOf(val);
+        } 
+    }      
+        return arr;
 };
 
 // Do not edit below this line
