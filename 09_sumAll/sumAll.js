2818bc3104d33a950bf8b8d829098b3695de4d2a
prathagupta01 <gpratha466@gmail.com>
2025-12-14 20:23:22 +0530

    completed :blush:

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(init,endy) {
+    let sum = 0
+    if(typeof init != 'number' || typeof endy != 'number') return "ERROR"
+    if(init < 0 || endy < 0) return 'ERROR'
+    if(!Number.isInteger(init) || !Number.isInteger(endy))return 'ERROR'
+    if(init > endy){
+        let temp = endy
+        endy = init 
+        init = temp
+    }
+    for(let i = init;i<= endy;i++){
+            sum += i;
+         }
+         return sum;
 };
 
 // Do not edit below this line
