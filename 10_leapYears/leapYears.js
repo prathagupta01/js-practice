34535439b9e95f4b27cada6dc1f36a57f026a10f
prathagupta01 <gpratha466@gmail.com>
2025-12-14 22:40:58 +0530

    completed :cheers: :heart: :blush:

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(yearr) {
+    // ------------------- first solution -------------------
+    // if(yearr % 4 === 0 && (yearr % 100 !== 0 ||yearr % 400 === 0)) return true
+    // else return false
+    // ------------------- second solution -------------------
+    if(yearr % 4 === 0 && (yearr % 100 !== 0)) return true
+    else if (yearr % 400 === 0) return true
+    else return false
 };
 
 // Do not edit below this line
