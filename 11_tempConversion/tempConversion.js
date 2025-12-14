53997e173438b54c644c46946531829b4b70d6ba
prathagupta01 <gpratha466@gmail.com>
2025-12-14 23:14:37 +0530

    completed :heart: :blush:

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempre) {
+  let celsius = (tempre - 32) * (5 / 9);
+  return Number(celsius.toFixed(1))
 };
-
-const convertToFahrenheit = function() {
+//If celsius = 36.666..., then Math.round(36.666... * 10) = Math.round(366.66...) = 367, and 367 / 10 = 36.7.
+const convertToFahrenheit = function(tempre) {
+  let fahrenheit = (tempre * (9 / 5)) + 32;
+  return Number(fahrenheit.toFixed(1))
 };
 
 // Do not edit below this line
