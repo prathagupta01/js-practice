3deee1b1126ebe736c735b992cb4aced9ca77486
prathagupta01 <gpratha466@gmail.com>
2025-12-21 16:38:12 +0530

    completed :blush: :beer:

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -7,6 +7,13 @@ const convertToFahrenheit = function(tempre) {
   let fahrenheit = (tempre * (9 / 5)) + 32;
   return Number(fahrenheit.toFixed(1))
 };
+// ------------------------------- Alternative solution ----------------------------
+// const convertToCelsius = function(fahrenheit) {
+//   return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
+// }; 
+// const convertToFahrenheit = function(celsius) {
+//   return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
+// };
 
 // Do not edit below this line
 module.exports = {
