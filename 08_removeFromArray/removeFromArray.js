1cbceeaba8614331751d64b43a92ee382eb76938
prathagupta01 <gpratha466@gmail.com>
2025-12-21 16:29:37 +0530

    completed :blush:

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -11,14 +11,14 @@ const removeFromArray = function(arr,...values) {
 
 // --------------------- SECOND METHOD -----------------------
 
-    // return arr.filter(v => (!values.includes(v)));
+    // return arr.filter(v => (!values.includes(v)));// filters out values that are in the values array .filter creates a new array with all elements that pass the test implemented by the provided function.
 
 // --------------------- THIRD METHOD  - MY METHOD-----------------------
 
     for(const val of values){
         let index = arr.indexOf(val);
         while(index !== -1){
-            arr.splice(index,1);
+            arr.splice(index,1); 
             index = arr.indexOf(val);
         } 
     }      
