b3d121b43c2d67a55ce18662a853d5e3fb4904ae
prathagupta01 <gpratha466@gmail.com>
2026-03-06 19:25:23 +0530

    completed :blush:

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a,b) {
+	   return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return b-a;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	  let total = 0 ;
+    for (let i = 0; i < array.length; i++) {
+      total += array[i];
+    } 
+    return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+    let product = 1 ;
+    for (let i = 0; i < array.length; i++) {
+      product *= array[i];
+    }
+    return product;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+  return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0 || a === 1) return 1;
+  else{
+    return a * factorial(a - 1);
+  }
 };
 
 // Do not edit below this line
