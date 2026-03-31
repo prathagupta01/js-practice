8fbf776355947e890661af24fd36b5e3663fff1f
prathagupta01 <gpratha466@gmail.com>
2026-03-31 17:10:07 +0530

    completed :heart:

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const alpha = "azqwsxedcrfvtgbyhnujmikolp1234567890";
+    const rev = str.toLowerCase().split("").filter(c => alpha.includes(c)).join("");
+    const revv = rev.split("").reverse().join("");
+    return rev === revv;
 };
 
 // Do not edit below this line
